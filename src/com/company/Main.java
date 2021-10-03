100f96f1fd0709bc4dad5b075b0174622cb0b1b5
DonjeZo <donjetazogaj2@gmail.com>
2021-10-03 19:43:58 +0200

    Removed the ressicable and visibile properteis of gameplay

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,8 +9,6 @@ public class Main {
         Gameplay gameplay = new Gameplay();
         obj.setBounds(10,10,700,600);
         obj.setTitle("Breakout Ball");
-        obj.setResizable(false);
-        obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         obj.add(gameplay);
     }
